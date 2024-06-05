c2e71a2c047bddc7c1120b36fa88803ffe4f1c10
JonathanKayombo <168289492+JonathanKayombo@users.noreply.github.com>
2024-06-05 13:58:09 +0200

    Left patterns HSProject.hs

--- HSProject.hs
+++ HSProject.hs
@@ -10,31 +10,18 @@ import System.Environment (getArgs)
 import System.IO (hFlush, stdout)
 
 -- EAN-13 patterns
-
 leftHandPatterns :: Map.Map Char String
-
 leftHandPatterns = Map.fromList [
-
     ('0', "0001101"), ('1', "0011001"), ('2', "0010011"), ('3', "0111101"),
-
     ('4', "0100011"), ('5', "0110001"), ('6', "0101111"), ('7', "0111011"),
-
     ('8', "0110111"), ('9', "0001011")
-
     ]
 
-
-
 rightHandPatterns :: Map.Map Char String
-
 rightHandPatterns = Map.fromList [
-
     ('0', "1110010"), ('1', "1100110"), ('2', "1101100"), ('3', "1000010"),
-
     ('4', "1011100"), ('5', "1001110"), ('6', "1010000"), ('7', "1000100"),
-
     ('8', "1001000"), ('9', "1110100")
-
     ]
     leftHandGPatterns :: Map.Map Char String
 leftHandGPatterns = Map.fromList [
